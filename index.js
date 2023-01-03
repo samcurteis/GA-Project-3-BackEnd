847f08b6540c12359ca624bd6e26108b9988f009
Jet <thazledine@hotmail.co.uk>
2023-01-03 12:55:13 +0000

    console logs requests

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express';
 
 const app = express()
 
+app.use((req, res) => {
+  console.log(`incoming request${req}`)
+})
 
 app.listen(3000, () => console.log('🤖 App is listening on port 3000'));
 
