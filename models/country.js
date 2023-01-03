0102a1fd77e7b80dfb9944be4cacf9d9a04df892
Nathan Harris <nathan.harris.1@outlook.com>
2023-01-03 17:23:41 +0000

    added seed data for countries to db

--- models/country.js
+++ models/country.js
@@ -1,7 +1,9 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const countrySchema = new mongoose.Schema({
-  name: { type: String, required: true }
+  name: { type: String, required: true },
+  code: { type: String, required: false },
+  continent: { type: String, required: false },
 });
 
-export default mongoose.model('country', countrySchema);
+export default mongoose.model("Country", countrySchema);
